e6e2d74c319dcee7e1133f3b6d45619222f51654
linzhou <970060731@qq.com>
2022-06-28 23:35:36 +0800

    库区功能开发中。。。。。。。。

---
+++ wms-base-service/src/main/java/com/wms/base/service/dao/stock/StockAreaMapper.java
@@ -0,0 +1,62 @@
+package com.wms.base.service.dao.stock;
+
+import com.wms.base.service.model.entity.stock.StockArea;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * ////////////////////////////////////////////////////////////////////
+ * //                          _ooOoo_                               //
+ * //                         o8888888o                              //
+ * //                         88" . "88                              //
+ * //                         (| ^_^ |)                              //
+ * //                         O\  =  /O                              //
+ * //                      ____/`---'\____                           //
+ * //                    .'  \\|     |//  `.                         //
+ * //                   /  \\|||  :  |||//  \                        //
+ * //                  /  _||||| -:- |||||-  \                       //
+ * //                  |   | \\\  -  /// |   |                       //
+ * //                  | \_|  ''\---/''  |   |                       //
+ * //                  \  .-\__  `-`  ___/-. /                       //
+ * //                ___`. .'  /--.--\  `. . ___                     //
+ * //              ."" '<  `.___\_<|>_/___.'  >'"".                  //
+ * //            | | :  `- \`.;`\ _ /`;.`/ - ` : | |                 //
+ * //            \  \ `-.   \_ __\ /__ _/   .-` /  /                 //
+ * //      ========`-.____`-.___\_____/___.-`____.-'========         //
+ * //                           `=---='                              //
+ * //      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        //
+ * //         佛祖保佑           永无BUG           永不修改              //
+ * //          佛曰:                                                  //
+ * //                 写字楼里写字间，写字间里程序员;                      //
+ * //                 程序人员写程序，又拿程序换酒钱.                      //
+ * //                 酒醒只在网上坐，酒醉还来网下眠;                      //
+ * //                 酒醉酒醒日复日，网上网下年复年.                      //
+ * //                 但愿老死电脑间，不愿鞠躬老板前;                      //
+ * //                 奔驰宝马贵者趣，公交自行程序员.                      //
+ * //                 别人笑我忒疯癫，我笑自己命太贱;                      //
+ * //                 不见满街漂亮妹，哪个归得程序员?                      //
+ * ////////////////////////////////////////////////////////////////////
+ *
+ * @date : 2022/6/28 23:13
+ * @author: linzhou
+ * @description : StockAreaMapper
+ */
+public interface StockAreaMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(StockArea record);
+
+    int insertSelective(StockArea record);
+
+    StockArea selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(StockArea record);
+
+    int updateByPrimaryKey(StockArea record);
+
+    int updateBatch(List<StockArea> list);
+
+    int updateBatchSelective(List<StockArea> list);
+
+    int batchInsert(@Param("list") List<StockArea> list);
+}
