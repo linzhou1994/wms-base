6b41680c9f5c149586b1f05f7acd5b8965bdccb4
林周 <970060731@qq.com>
2022-07-09 17:22:50 +0800

    新增库位管理

--- wms-base-api/src/main/java/com/wms/base/api/interceptor/handler/CompanyWarehouseHandler.java
+++ wms-base-api/src/main/java/com/wms/base/api/interceptor/handler/CompanyWarehouseHandler.java
@@ -6,12 +6,16 @@ import com.wms.base.api.dto.warehouse.LoginWarehouseDTO;
 import com.wms.base.api.remote.company.RemoteCompanyService;
 import com.wms.base.api.remote.warehouse.RemoteWarehouseService;
 import com.wms.base.api.utils.LoginWarehouseUtils;
+import com.wms.singlesignonapi.annotation.NotLogin;
 import com.wms.singlesignonapi.interceptor.handler.LoginHandler;
+import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Objects;
 
 /**
  * CopyRight : <company domain>
@@ -35,10 +39,17 @@ public class CompanyWarehouseHandler implements LoginHandler {
 
     @Override
     public void preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        LoginCompanyDTO loginCompany = remoteCompanyService.getLoginCompany();
-        LoginWarehouseDTO loginWarehouse = remoteWarehouseService.getLoginWarehouse();
-        LoginWarehouseUtils.setLoginWarehouse(loginWarehouse);
-        LoginWarehouseUtils.setLoginCompany(loginCompany);
+        if (handler instanceof HandlerMethod) {
+            HandlerMethod method = (HandlerMethod) handler;
+            NotLogin notLogin = AnnotationUtils.findAnnotation(method.getMethod(), NotLogin.class);
+            if (Objects.nonNull(notLogin)) {
+                return;
+            }
+            LoginCompanyDTO loginCompany = remoteCompanyService.getLoginCompany();
+            LoginWarehouseDTO loginWarehouse = remoteWarehouseService.getLoginWarehouse();
+            LoginWarehouseUtils.setLoginWarehouse(loginWarehouse);
+            LoginWarehouseUtils.setLoginCompany(loginCompany);
+        }
     }
 
     @Override
